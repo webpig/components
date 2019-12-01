51ab9c167b4b973b15aa16ff61dca84f52fa147a
webpig <11765461@qq.com>
2019-12-01 14:00:09 +0800

    chore: 打包，添加注释

--- src/examples/SelectExample.vue
+++ src/examples/SelectExample.vue
@@ -1,5 +1,13 @@
 <template>
     <div>
+        <!-- 
+            data 数据列表，数组类型，每项都为对象，比如{ id: 1, name: '张三' }
+            label 选择器显示字段，数据项的属性名，比如name
+            add-ben-text 添加按钮的文本
+            input-label 输入框的label
+            confirm 确认添加新数据事件
+            change 选择数据事件，返回选择的数据
+        -->
         <Select
             :data="data"
             label="name"
